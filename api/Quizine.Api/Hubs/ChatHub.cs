81c11b802554b398ef001d567eb7f9ad9d422f63
André Toll <Andre.Toll@c-rad.com>
2021-08-09 10:57:18 +0200

    Added api and client projects

---
+++ api/Quizine.Api/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Quizine.Api.Hubs.Clients;
+using Quizine.Api.Models;
+using System.Threading.Tasks;
+
+namespace Quizine.Api.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
---
+++ api/Quizine.Api/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,10 @@
+﻿using Quizine.Api.Models;
+using System.Threading.Tasks;
+
+namespace Quizine.Api.Hubs.Clients
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+    }
+}
