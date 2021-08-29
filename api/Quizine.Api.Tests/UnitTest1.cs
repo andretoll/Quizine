4a0b90a50dd72bafc0cff672f1bf013ba0c45b06
André Toll <Andre.Toll@c-rad.com>
2021-08-29 20:16:31 +0200

    Added unit tests project. Added workflow for running tests.

---
+++ api/Quizine.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Quizine.Api.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true);
+        }
+    }
+}
